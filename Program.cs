373e0b4c2f7ff12e9eb820216d629ec424220c2c
vitalikkov <106430549+Vitalikkov@users.noreply.github.com>
2023-02-18 19:13:27 +0200

    add
    add

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = Convert.ToUInt32(Console.ReadLine());
+
+            int k = 0;
+
+            while (Math.Pow(2, k) <= n)
+            {
+                k++;
+            }
+            Console.WriteLine(k);
+        }
+    }
+}
